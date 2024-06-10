c72526797042358183a8f740d1aebc963c5b2110
Adarsh <ashu2100ag@gmail.com>
2024-06-10 11:36:55 +0530

    Moved project files and completed Tailwind

--- Project/Js/Text Editor/Text Editor.js
+++
@@ -1,8 +0,0 @@
-let textarea = document.querySelector("#textarea");
-console.dir(textarea);
-let p = document.querySelector("p");
-
-textarea.addEventListener("input", function () {
-    // console.log(textarea.value);
-    p.innerText = textarea.value;
-});
