52db0784fbdd5122735bec782b587af64aaf2567
Adarsh <ashu2100ag@gmail.com>
2024-06-10 23:39:10 +0530

    Complete Tailwind Css

---
+++ FRONTEND/Code with Harry/Tailwind Css/Lecture 14/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        'xsm':'200px',
+      },
+      colors: {
+        'lime-green': '#42b72a',
+    },
+  },
+  plugins: [],
+}
+}
