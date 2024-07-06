48b144e4d50f44db77a82fc3781dd4407d7d65da
Adarsh <ashu2100ag@gmail.com>
2024-07-06 23:13:21 +0530

    Started React from Harry

---
+++ FRONTEND/Code with Harry/React/React Lecture-4/lecture-4/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name="Adarsh";
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src="" alt="" />
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reprehenderit 
+        iste quis recusandae natus. Laborum ratione nam eius rem! Dicta, recusandae odit! Voluptatibus, 
+        libero ipsam. Incidunt provident earum et rerum!
+      </p>
+    </div>
+    </>
+  );
+}
+
+export default App;
