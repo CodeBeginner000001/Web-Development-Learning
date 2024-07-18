2d6b0a7a6a243fc0b9592760f29020a62e74b475
Adarsh <ashu2100ag@gmail.com>
2024-07-19 00:33:17 +0530

    completed expressjs

--- BACKEND/NodeJs/Lecture-2 (ExpressJs)/index.js
+++ BACKEND/NodeJs/Lecture-2 (ExpressJs)/index.js
@@ -47,21 +47,62 @@ app.listen(port,()=>{
             http://localhost:3000 or http://localhost:8080
     
     */
-    app.get("/",(req,res)=>{
-        res.send("You have contacted root node");
-    })
-    app.get("/apple",(req,res)=>{
-        res.send("You have contacted apple node");
-    })
-    app.get("/lemon",(req,res)=>{
-        res.send("You have contacted lemon node");
-    })
-    app.get("/orange",(req,res)=>{
-        res.send("You have contacted orange node");
-    })
-    app.get("*",(req,res)=>{
-        res.send("This path doesn't exist");
-    })
+    // app.get("/",(req,res)=>{
+    //     res.send("You have contacted root node");
+    // })
+    // app.get("/apple",(req,res)=>{
+    //     res.send("You have contacted apple node");
+    // })
+    // app.get("/lemon",(req,res)=>{
+    //     res.send("You have contacted lemon node");
+    // })
+    // app.get("/orange",(req,res)=>{
+    //     res.send("You have contacted orange node");
+    // })
+    // app.get("*",(req,res)=>{
+    //     res.send("This path doesn't exist");
+    // })
     // app.post("/",(req,res)=>{
     //     res.send("You sent a post");
     // })
+
+    /* Understanding about path parameter */
+    /* Used for one parameter */
+// app.get("/:username",(req,res)=>{
+//     console.log(req.params);
+//     res.send("Hello, I am root");
+// })
+
+    /* Used for two parameter */
+// app.get("/:username/:id",(req,res)=>{
+//     console.log(req.params);
+//     res.send("Hello, I am root");
+// })
+
+   /* Using those params */
+//    app.get("/",(req,res)=>{
+//     res.send("Hello, I am root");
+//    })
+//    app.get("/:username",(req,res)=>{
+//     let {username}=req.params;
+//     let htmlWelcome = `<h1>Welcome to the page of @${username}</h1>`
+//     res.send(htmlWelcome)
+//    })
+
+/* Query Strings */
+   app.get("/",(req,res)=>{
+    res.send("Hello, I am root");
+   })
+   app.get("/:username/:id",(req,res)=>{
+    let {username}=req.params;
+    let htmlWelcome = `<h1>Welcome to the page of @${username}</h1>`
+    res.send(htmlWelcome)
+   })
+   app.get("/search",(req,res)=>{
+    let {q}=req.query;
+    if(!q){
+        res.send("Nothing searched");
+    }else{
+    res.send(`Search results for query: ${q}`);
+    }
+   })
