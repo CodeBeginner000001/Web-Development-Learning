46e80f84c11d31bba5cc995ece75f00fcde2132e
Adarsh <ashu2100ag@gmail.com>
2024-12-05 01:46:06 +0530

    compeleted Lecture 49

--- BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/routes/auth.js
+++ BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/routes/auth.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const User = require('../models/User')
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const JWT_Secret = "ggwellplayedmylovedaring"
 // Below line is used from the npm package called xpress-validator
 const { body, validationResult } = require("express-validator");
 
@@ -21,15 +25,30 @@ router.post("/createuser", [
         /** Return the Valid user response and check wheather the user with this email exists already*/
         let user = await User.findOne({ email: req.body.email })
         if (user) return res.status(400).json({ error: "Sorry a user with this email already exists" })
+
+        /*
+         Securing password using bcrypt and salt, now salt is what u add to the password and 
+         generate hashcode for the passwor 
+         */ 
+        const salt = await bcrypt.genSalt(16);
+        const securedPassWord =await bcrypt.hash(req.body.password,salt);
+        // Creating new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: securedPassWord,
             email: req.body.email,
         })
+        /** using JWT token to create a token for the user */
+        const data = {
+            user:{
+                id:user.id,
+            }
+        }
+        const AuthToken = jwt.sign(data, JWT_Secret)
+        res.json({AuthToken});
         // res.send("Authentication");
         // const user = User(req.body);
         // user.save();
-        res.status(201).json(user);
     } catch (error) {
         res.status(500).send("Internal Server Error")
     }
