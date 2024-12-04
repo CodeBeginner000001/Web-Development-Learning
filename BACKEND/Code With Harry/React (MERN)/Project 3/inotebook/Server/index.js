3be0c2f1555bc17415ffd22ea79469087f4ab4a9
Adarsh <ashu2100ag@gmail.com>
2024-12-04 23:13:09 +0530

    Complete Lecture 44

--- BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/index.js
+++ BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/index.js
@@ -3,6 +3,9 @@ const express = require("express")
 const app = express();
 const port = 3000;
 connectToMongo();
+// Available Routed
+app.use("/api/auth", require('./routes/auth'))
+app.use("/api/notes", require('./routes/notes'))
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
