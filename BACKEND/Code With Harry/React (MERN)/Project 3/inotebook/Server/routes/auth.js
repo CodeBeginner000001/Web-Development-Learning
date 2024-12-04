542a58618f17cb1986f8d6bd35ac5a9a72e9a51c
Adarsh <ashu2100ag@gmail.com>
2024-12-05 00:42:24 +0530

    compeleted Lecture 47

--- BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/routes/auth.js
+++ BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/Server/routes/auth.js
@@ -2,34 +2,36 @@ const express = require("express")
 const User = require('../models/User')
 const router = express.Router();
 // Below line is used from the npm package called xpress-validator
-const {body, validation, validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post("/",[
-    body("name","Enter a Valid Name").isLength({min:3}),
-    body("email","Enter a valid Email").isEmail(),
-    body("password","Password must be atleast 8 character").isLength({min:5}),
-],(req,res)=>{
-    console.log(req.body);
-    /** checking for errors from validator */
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+// Create a User using: POST "/api/auth/createuser". Doesn't require Auth i.e., no login required
+router.post("/createuser", [
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be atleast 8 character").isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+        // console.log(req.body);
+        /** checking for errors from validator , return bad request and the errors */
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        /** Return the Valid user response and check wheather the user with this email exists already*/
+        let user = await User.findOne({ email: req.body.email })
+        if (user) return res.status(400).json({ error: "Sorry a user with this email already exists" })
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+        // res.send("Authentication");
+        // const user = User(req.body);
+        // user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
     }
-    /** Return the Valid user response */
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    })
-    .then(user => res.json(user))
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: "Please Enter a unqiue value for email" ,message:err.message});
-    });
-    // res.send("Authentication");
-    // const user = User(req.body);
-    // user.save();
 });
-module.exports = router
+module.exports = router 
