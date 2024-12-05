3bafe98b2ec567a5c4a26499d88c494275691240
Adarsh <ashu2100ag@gmail.com>
2024-12-05 22:40:20 +0530

    Completed Lecture 58

--- BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/src/components/About.js
+++ BACKEND/Code With Harry/React (MERN)/Project 3/inotebook/src/components/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/NoteContext'
 
 const About = () => {
+  const a = useContext(NoteContext)
+  useEffect(()=>{
+    a.update();
+  },[])
   return (
     <div>
-      This is about
+      This is about {a.state.name} and my age is {a.state.age}
     </div>
   )
 }
